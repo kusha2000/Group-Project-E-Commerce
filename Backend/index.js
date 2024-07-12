d71b152a4d5dd1b6705398962c42b65aee0fad5f
kusha2000 <kushanandarawewa1@gmail.com>
2024-07-12 11:26:00 +0530

    Configure MongoDB and Backend Configuration

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,18 @@
-console.log("backend");
+const express=require('express')
+require('dotenv').config()
+const connectDB=require('./config/db')
+
+const app = express();
+const PORT =3000 || process.env.PORT
+
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log("Connect to DB");
+        console.log("Server is running");
+    })
+})
