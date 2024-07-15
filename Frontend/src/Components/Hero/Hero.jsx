cba3b264cb2b32bc24bf871e029b800fe2b77c27
Rumanda Senanayake <rumandasenanayake@gmail.com>
2024-07-15 20:46:24 +0530

    Hero Section

---
+++ Frontend/src/Components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Hero.css";
+import hero from "../Assets/hero_image.png";
+import arrow_icon from "../Assets/arrow.png";
+import hand_icon from "../Assets/hand_icon.png";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-left">
+       
+        <div>
+        <p>New Collections for everyone</p>
+        </div>
+
+        <div className="hero-latest-btn">
+          <div>Latest Collection</div>
+          <img src={arrow_icon} alt="" />
+        </div>
+      </div>
+      <div className="hero-right">
+        <img src={hero} alt="hero" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
