4afd2e12aac31514ec21ab4e01805c6a9d630623
kusha2000 <kushanandarawewa1@gmail.com>
2024-07-16 09:52:40 +0530

    Create the API for Getting the products and deleting the products

--- Backend/index.js
+++ Backend/index.js
@@ -79,9 +79,19 @@ const Product =mongoose.model("Product",{
   },
 })
 
+//Add Products
 app.post('/addproduct',async (req,res)=>{
+  let products=await Product.find({});
+  let id;
+  if(products.length >0){
+    let last_product_array=products.slice(-1);
+    let last_product=last_product_array[0];
+    id=last_product.id+1;
+  }else{
+    id=1;
+  }
   const product=new Product({
-    id:req.body.id,
+    id:id,
     name:req.body.name,
     image:req.body.image,
     category:req.body.category,
@@ -95,6 +105,26 @@ app.post('/addproduct',async (req,res)=>{
     success:true,
     name:req.body.name,
   })
+})
+
+// Delete Products
+app.post('/removeproduct',async (req,res)=>{
+  await Product.findOneAndDelete({
+    id:req.body.id
+  })
+  console.log("Deleted the product");
+  res.json({
+    success:true,
+    name:req.body.name
+  })
+})
+
+//Getting All Products
+app.get('/allproduct',async (req,res)=>{
+  let product=await Product.find({});
+  console.log("All Products Fetched");
+
+  res.send(product)
 })
 
 
