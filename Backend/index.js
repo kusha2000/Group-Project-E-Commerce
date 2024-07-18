f9da16753d099fa7306377a4115ba668ce03954e
kusha2000 <kushanandarawewa1@gmail.com>
2024-07-18 09:10:09 +0530

    Add User Login and Register API

--- Backend/index.js
+++ Backend/index.js
@@ -150,6 +150,89 @@ const Users=mongoose.model('Users',{
   }
 })
 
+//Register User
+app.post('/signup',async (req,res)=>{
+  let check=await Users.findOne({email:req.body.email});
+
+  if(check){
+    return res.status(400).json({success:false,message:"existing user found with same email"})
+  }
+  let cart={}
+  for (let i = 0; i < 300; i++) {
+    cart[i]=0; 
+  }
+  const user =new Users({
+    name:req.body.username,
+    email:req.body.email,
+    password:req.body.password,
+    cartData:cart,
+  })
+
+  await user.save();
+  const data={
+    user:{
+      id:user.id
+    }
+  }
+
+  const token=jwt.sign(data,'secret_ecom');
+  res.json({success:true,token})
+
+})
+
+//user login
+app.post('/login',async (req,res)=>{
+  let user=await Users.findOne({email:req.body.email});
+  if(user){
+    const passCompare=req.body.password === user.password;
+    if(passCompare){
+      const data={
+        user:{
+          id:user.id
+        }
+      }
+      const token = jwt.sign(data,'secret_ecom');
+      res.json({success:true,token});
+
+    }else{
+      res.json({success:false,errors:"Wrong Password"});
+    }
+  }else{
+    res.json({success:false,errors:"Wrong Email ID"})
+  }
+})
+
+//newCollection Data
+app.get('/newcollection',async (req,res)=>{
+  let products=await Product.find({});
+  let newCollection=products.slice(1).slice(-8);
+  console.log("NewCollection Fetched");
+  res.send(newCollection);
+})
+
+//newCollection Data
+app.get('/popularinwomen',async (req,res)=>{
+  let products=await Product.find({category:"women"});
+  let popular_in_women=products.slice(0,4);
+  console.log("Popular in women fetched");
+  res.send(popular_in_women);
+})
+
+//create a middleware to fetch user
+const fetchUser=async(req,res,next)=>{
+  const token =req.header('auth-token');
+  if(!token){
+    res.status(401).send({error:"No token Given"})
+  }else{
+    try {
+      const data=jwt.verify(token,'secret_ecom');
+      req.user = data.user;
+      next();
+    } catch (error) {
+      res.status(401).send({errors:"Please authenticate using valid token"})
+    }
+  }
+}
 
 
 
